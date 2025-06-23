d30b8c87f9c644cdc87f0e7494c86eabbe308e5e
stellalphatic <beingshahzaib26@gmail.com>
2025-06-23 10:32:21 +0500

    built  PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#BE29EC" />
       </head>
       <body className={cn("font-sans antialiased", "bg-background text-foreground")}>
         <AuthProvider>
